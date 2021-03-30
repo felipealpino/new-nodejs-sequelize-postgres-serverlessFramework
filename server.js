2feb7b977374dfb1995895b4f4e905de2ac93e43
Felipe Alpino <felipealpino1@gmail.com>
2021-03-29 21:05:05 -0300

    configurando variaveis de ambiente

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./app') // recebe o app que está sendo exportado de app.js
+require('dotenv').config({
+    path:variables.env 
+}) //definindo variables.env como o arquivo para acesso de process.env das variaveis de ambiente
+
+app.set('port', process.env.PORT || 7777) //setando porta servidor na 7777
+const server = app.listen(app.get('port'), () => { 
+    console.log('Server is runnung on: '+server.address().port)
+})
